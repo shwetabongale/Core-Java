abff8e9d026db902b2930f2f370d32b9a139a7dc
shwetabongale <shwetabongale19@gmail.com>
2025-06-17 14:05:32 +0530

    initial commit

---
+++ Sports.java
@@ -0,0 +1,9 @@
+class Sports{
+	public static void main(String[] args){
+	System.out.println("sports keep us active");
+	System.out.println("sports keep us healthy");
+	System.out.println("sports are essential");
+	System.out.println("sports give us strength");
+	System.out.println("sports give a strategy");
+	}
+}
